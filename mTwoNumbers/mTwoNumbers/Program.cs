64857d1ef5f109f045625317e2fc72f44ca6055f
Twilight <17354422606@163.com>
2018-09-11 22:13:28 +0800

    homework1

---
+++ mTwoNumbers/mTwoNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1 = "", num2 = "";
+            Console.WriteLine("Please input a numbers :");
+            num1 = Console.ReadLine();
+            Console.WriteLine("Please input a number again :");
+            num2 = Console.ReadLine();
+            int _num1 = Int32.Parse(num1);
+            int _num2 = Int32.Parse(num2);
+            int num = _num1 * _num2;
+            Console.WriteLine("Product of two numbers :" + num);
+        }
+    }
+}
